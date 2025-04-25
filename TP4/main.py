531b15db74e29d1e776782bc2012059a7b9f9d96
Mathis <mathis.delmaere@free.fr>
2025-04-25 12:08:18 +0200

    TP4

---
+++ TP4/main.py
@@ -0,0 +1,166 @@
+from typing import List, Tuple, Callable
+import ast
+import random
+
+# Optionnel
+import itertools as it
+import time
+
+# Quelques structures de données
+Grid = tuple[tuple[int, ...], ...]   # tuple de tuple pour pouvoir le mettre en cache dans un dictionnaire (car pas mutable) + garde l'idée qu'on peut créer un nouvel état mais pas modifier l'état actuel
+State = Grid   # Grille actuelle
+Action = tuple[int, int]  # Les coords (qui sont suffisantes avec l'info Player)
+Player = int   # 1 pour x et 2 pour o
+Score = float  # Pour faire une fonctione d'évaluation à profondeur limitée
+Strategy = Callable[[State, Player], Action]
+
+# Quelques constantes
+DRAW = 0
+EMPTY = 0
+X = 1
+O = 2
+
+EMPTY_GRID: Grid = ((0, 0, 0), (0, 0, 0), (0, 0, 0))
+GRID_0: Grid = EMPTY_GRID
+GRID_1: Grid = ((0, 0, 0), (0, X, O), (0, 0, 0))
+# (0, 0, 0),
+# (0, X, O),
+# (0, 0, 0))
+
+GRID_2: Grid = ((O, 0, X), (X, X, O), (O, X, 0))
+#((O, 0, X),
+# (X, X, O),
+# (O, X, 0)
+
+GRID_3: Grid = ((O, 0, X), (0, X, O), (O, X, 0))
+#((O, 0, X),
+# (0, X, O),
+# (O, X, 0))
+
+GRID_4: Grid = ((0, 0, 0), (X, X, O), (0, 0, 0))
+#((0, 0, 0),
+# (X, X, O),
+# (0, 0, 0))
+
+GRID_5: Grid = ((0, O, O), (X, O, X), (O, 0, 0))
+
+def grid_tuple_to_grid_list(grid: Grid) -> list[list[int]]:
+    return [list(tuple) for tuple in [mother_tuple for mother_tuple in grid]]
+
+def grid_list_to_grid_tuple(grid: list[list[int]]) -> Grid:
+    return tuple([tuple(liste) for liste in [mother_liste for mother_liste in grid]])
+
+def legals(grid: State) -> list[Action]:
+    legs : list[Action] = []
+    for line in range(3):
+        for col in range(3):
+            if(grid[line][col] == 0):
+                legs.append((line, col))
+    return legs
+
+def check_lines(grid: State, player: Player) -> bool:
+    count: int = 0
+    for line in range(3):
+        count = 0
+        for col in range(3):
+            if(grid[line][col] == player):
+                count+=1
+            if count == 3:
+                return True
+    return False
+
+def check_cols(grid: State, player: Player) -> bool:
+    count: int = 0
+    for line in range(3):
+        count = 0
+        for col in range(3):
+            if(grid[col][line] == player):
+                count+=1
+            if count == 3:
+                return True
+    return False
+
+def check_diags(grid: State, player: Player) -> bool:
+    count: int = 0
+    if(grid[i][i] == player for i in range (3)):
+        return True
+    if(grid[i][2-i] == player for i in range (3)):
+        return True
+    return False
+
+def line(grid: State, player: Player) -> bool:
+    return check_lines(grid, player) or check_cols(grid, player) or check_diags(grid, player)
+
+def final(grid: State) -> bool:
+    full:bool = (grid[i][j] != 0 for i, j in[range(3), range(3)])
+    return full or line(grid, X) or line(grid, O)
+
+def score(grid: State) -> Score:
+    if line(grid, X):
+        return 1
+    elif line(grid, O):
+        return -1
+    else:
+        return 0
+
+def pprint(grid: State):
+   for line in range(3):
+        for col in range(3):
+            if(grid[line][col]==0):
+                print('.', end=' ') 
+            elif(grid[line][col]==1):
+                print('X', end=' ') 
+            else:
+                print('O', end=' ') 
+        print()
+
+def play(grid: State, player: Player, action: Action) -> State:
+    new_state:[[int]] = grid_tuple_to_grid_list(grid)
+    new_state[action[0]][action[1]] = player
+    return grid_list_to_grid_tuple(new_state)
+
+def tictactoe(strategy_X: Strategy, strategy_O: Strategy, debug: bool = False) -> Score:
+    grid: State = EMPTY_GRID
+    pprint(grid)
+    player: Player = X
+    while(final(grid)!=False):
+        if player == X:
+            play(grid, X, strategy_X(grid, X))
+            player = O
+        else: 
+            play(grid, O, strategy_O(grid, O))
+            player = X
+        pprint(grid)
+    return score(grid)
+
+def strategy_first_legal(grid: State, player: Player) -> Action:
+    return(legals(grid)[0])
+
+def strategy_random(grid: State, player: Player) -> Action:
+    legs: list[Action] = legals(grid)
+    return (legs[random.randint(0, len(legs)-1)])
+
+def minmax(grid: State, player: Player) -> Score:
+    pass # Écrire la fonction minmax qui étant donné un état de jeu renvoie le score optimal de la partie.
+
+def minmax_action(grid: State, player: Player, depth: int = 0) -> tuple[Score, Action]:
+    pass # Écrire la fonction minmax qui étant donné un état de jeu renvoie sous forme de tuple l'action amenant au score optimal de la partie et ce score.
+
+def strategy_minmax(grid: State, player: Player) -> Action:
+    pass # En déduire la fonction suivante.
+
+def minmax_actions(grid: State, player: Player, depth: int = 0) -> tuple[Score, list[Action]]:
+    pass # Écrire la fonction minmax qui étant donné un état de jeu renvoie dans un tuple les actions amenant au score optimal de la partie et ce score.
+
+# Utiliser cette fonction pour créer un minmax indéterministe.
+def strategy_minmax_random(grid: State, player: Player) -> Action:
+    pass
+
+# Ajouter un cache aux fonctions précédentes en utilisant le pattern de mémoïsation.
+# Mesurer le résultat avec time
+
+def main():
+    tictactoe(strategy_first_legal,strategy_random)
+
+if __name__ == "__main__":
+    main()
